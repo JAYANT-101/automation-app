481e69e135233b2fafe820b6616245d435c803e3
JAYANT-101 <jayantsinha2005@gmail.com>
2026-05-02 14:25:16 +0530

    update

--- Python-backhand/web_app/users/users.py
+++ Python-backhand/web_app/users/users.py
@@ -4,11 +4,21 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from web_app.auth import login_required
 from web_app.data_utils import insert_user_in_users_table, get_all_users_data, delete_user_by_username
 
+def show_users():
+    """This function shoes all users"""
+    try:
+        users = get_all_users_data()
+    except Exception as e:
+        flash(str(e))
+    data = [name for _,name,*_ in users]
+    return data
+
 bp = Blueprint('users', __name__, url_prefix='/users')
 
 bp.route('/add_user', methods=('GET','POST'))
 def add_user():
     """This function adds user that are going to be using the checker application on the app"""
+    data = show_users()
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
@@ -25,18 +35,11 @@ def add_user():
             else:
                 return redirect(url_for("auth.login"))
         flash(error)
-    return render_template('users.add_user')
-bp.route('/show_users')
-def show_users():
-    try:
-        users = get_all_users_data()
-    except Exception as e:
-        flash(str(e))
-    data = [name for _,name,*_ in users]
-    return render_template('users.show_users', data=data)
+    return render_template('users.add_user', data=data)
 
 bp.route('/delete_user')
 def delete_user(username: str):
+    data = show_users()
     if request.method == 'POST':
         username = request.form['username']
         error = None
@@ -48,4 +51,4 @@ def delete_user(username: str):
             except Exception as e:
                 error = f"User {username} dose not exist"
         flash(error)
-    return render_template('users.delete_user')
+    return render_template('users.delete_user', data=data)
